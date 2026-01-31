60aa3e1f251fcec21bbc558def18be084bb9c2af
Harsh Bisht <harshbisht113322@gmail.com>
2026-01-31 22:11:43 +0530

    Adding Service layer in the Web Service

---
+++ AuthService/src/main/java/com/harshbisht/AuthService/external/UserFeignClient.java
@@ -0,0 +1,13 @@
+package com.harshbisht.AuthService.external;
+
+import com.harshbisht.AuthService.config.FeignConfig;
+import com.harshbisht.AuthService.dto.UserEntity;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "USER-SERVICE", configuration = FeignConfig.class)
+public interface UserFeignClient {
+    @PostMapping("/users")
+    UserEntity createUser(@RequestBody UserEntity user);
+}
