4a85f866a1262933b7f23baeeffdc7ecfe614b37
Harsh Bisht <harshbisht113322@gmail.com>
2026-03-22 10:52:49 +0530

    Basic Service Structure

--- ResultService/src/main/java/com/harshbisht/ResultService/controller/ResultController.java
+++ ResultService/src/main/java/com/harshbisht/ResultService/controller/ResultController.java
@@ -2,13 +2,54 @@ package com.harshbisht.ResultService.controller;
 
 import com.harshbisht.ResultService.service.ResultService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/result")
+@RequestMapping("/results")
 @RequiredArgsConstructor
 public class ResultController {
 
     private final ResultService resultService;
+
+    // APIs
+
+    /*
+    Accepting Exam Submissions
+    Scoring Logic
+    Prevent Double Submission
+    Fetch Student’s Results
+    Fetch Result For Particular Exam
+    Teacher View – View Results of Their Exam
+
+    Student
+
+    POST /results/submit
+    GET /results/my
+    GET /results/exam/{examId}
+
+    Teacher
+    GET /results/exam/{examId}/all
+    */
+//    @PostMapping("/submit")
+//    public ExamSubmitResponse submitExam(@RequestBody ExamSubmitRequest request) {
+//        return resultService.submitExam(request);
+//    }
+//
+//    @GetMapping("/my")
+//    public List<ResultResponse> GetAllResults () {
+//        // get studentId by security context
+//        return resultService.getAllResults();
+//    }
+//
+//    @GetMapping("exam/{examId}")
+//    public ResultResponse getResult(@PathVariable Long examId) {
+//        return resultService.getResult(examId);
+//    }
+//
+//    @GetMapping("/exam/{examId}/all")
+//    public List<ResultResponse> getResultOfStudentsByExam(@PathVariable Long examId) {
+//        return resultService.getResultOfStudentsByExam(examId);
+//    }
 }
